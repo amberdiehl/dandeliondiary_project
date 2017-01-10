6febdb13c7af9a73523071120a62e8b53e311438
amberdiehl <virtuallyad@gmail.com>
2017-01-10 10:49:36 -0700

    get aws working for static files

--- dandeliondiary/dandeliondiary/settings/base.py
+++ dandeliondiary/dandeliondiary/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     'django_q',
     'bootstrap3',
     'account',
