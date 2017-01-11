9589e373ef3365a5a88042538476b6cce9c13ac7
amberdiehl <virtuallyad@gmail.com>
2017-01-11 10:20:45 -0700

    use aws cloudfront for static and media files

--- dandeliondiary/custom_storages.py
+++ dandeliondiary/custom_storages.py
@@ -5,6 +5,32 @@ from storages.backends.s3boto import S3BotoStorage
 class StaticStorage(S3BotoStorage):
     location = settings.STATICFILES_LOCATION
 
+    def _clean_name(self, name):
+        return name
+
+    def _normalize_name(self, name):
+        if not name[0] == '/':
+            name = '/' + name
+        # if not name.endswith('/'):
+        #    name += "/"
+
+        # name += self.location
+        name = self.location + name
+        return name
+
 
 class MediaStorage(S3BotoStorage):
     location = settings.MEDIAFILES_LOCATION
+
+    def _clean_name(self, name):
+        return name
+
+    def _normalize_name(self, name):
+        if not name[0] == '/':
+            name = '/' + name
+        # if not name.endswith('/'):
+        #    name += "/"
+
+        # name += self.location
+        name = self.location + name
+        return name
--- dandeliondiary/dandeliondiary/settings/base.py
+++ dandeliondiary/dandeliondiary/settings/base.py
@@ -154,14 +154,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, '..', "static"),)
-STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, '..', "static"),)
+# STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(MAIN_DIR, 'media')
-MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(MAIN_DIR, 'media')
+# MEDIA_URL = '/media/'
 
-CONTENT_TYPES = ['image', 'video']
-MAX_UPLOAD_SIZE = "5242880"
+# CONTENT_TYPES = ['image', 'video']
+# MAX_UPLOAD_SIZE = "5242880"
