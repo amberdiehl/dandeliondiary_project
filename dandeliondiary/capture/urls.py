eefb87acbd14bdcb65f3c6e2e56d1115a95d7be5
amberdiehl <virtuallyad@gmail.com>
2017-02-08 09:46:51 -0700

    feature/navigation-show-active-selection

--- dandeliondiary/capture/urls.py
+++ dandeliondiary/capture/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^expenses', views.explore_expenses, name="explore_expenses"),
+    url(r'^new', views.new_expense, name="new_expense"),
+    url(r'^explore', views.explore_expenses, name="explore_expenses"),
     url(r'^ajax/list_expenses/$', views.ajax_list_expenses),
     url(r'^ajax/change_expense/(?P<s>\w+)/?', views.ajax_change_expense),
-    url(r'^', views.new_expense, name="new_expense"),
 ]
