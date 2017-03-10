a1de9c9692579d77f98496707fc4269f3fdb93c9
amberdiehl <virtuallyad@gmail.com>
2017-03-10 16:38:34 -0700

    fabric for producution deploys

--- dandeliondiary/fabfile.py
+++ dandeliondiary/fabfile.py
@@ -1,25 +1,31 @@
-from fabric.api import local, abort
+from __future__ import with_statement
+from fabric.api import abort, local, settings
 from fabric.contrib.console import confirm
 
 """
 IMPORTANT: Deployment assumes all feature branches ready for production have been committed and merged into master
+           and that the user is currently on master to indicate readiness.
 """
 
 
 def print_message(msg):
 
     print(' ')
-    print('==> ' + msg + '...')
+    print('==> ' + msg)
     print(' ')
 
 
 def check_local_branch():
-
-    print_message("Checking local branch")
+    """
+    Validate user is on master branch
+    :return:
+    """
+    print_message("Validating current branch:")
     my_branch = local('git rev-parse --abbrev-ref HEAD', capture=True)
     print("Current branch is: %s" % my_branch)
     if my_branch != 'master':
-        abort('Please ensure all changes ready for release are committed to master and make master current branch.')
+        abort('Please ensure all changes ready for release are committed and merged with master and make master '
+              'working branch.')
 
 
 def run_tests():
@@ -41,14 +47,78 @@ def push():
     local("git push origin")
 
 
+def ssh():
+    """
+    Login into PythonAnywhere host
+    :return:
+    """
+    local("ssh amberdiehl@ssh.pythonanywhere.com")
+    # password must be supplied manually
+
+
+def pull():
+    """
+    Pull changes for deployment
+    :return:
+    """
+    local("git pull origin")
+    # credentials must be supplied manually
+
+
+def run_migrations():
+    """
+    Determine if there are migrations to run and, if so, run them
+    :return:
+    """
+    with settings(warn_only=True):
+        migrations = local("./manage.py showmigrations --list --settings=dandeliondiary.settings.production "
+                           "| grep '\[ ]'", capture=True)
+    if migrations.failed:
+        print_message("No migrations found.")
+    else:
+        print_message("Applying migrations...")
+        local("./manage.py migrate --settings=dandeliondiary.settings.production")
+
+
+def restart_server():
+    """
+    Restart the server
+    :return:
+    """
+    local("touch /var/www/www_dandeliondiary_com_wsgi.py")
+
+
+def exit_ssh():
+    """
+    Exit from ssh session
+    :return:
+    """
+    local("exit")
+
+
+"""
+Main scripts
+"""
+
+
 def deploy():
     """
-    Deployment assumes that all feature branches ready for deployment have already been merged to master
+    Deploy latest release/changes for Dandelion Diary to production
     :return:
     """
 
-    run_tests()
-
     check_local_branch()
 
+    run_tests()
+
     push()
+
+    ssh()
+
+    pull()
+
+    run_migrations()
+
+    restart_server()
+
+    exit_ssh()
